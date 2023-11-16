3f2df7ba7e3620cfe7d5b8bd39ed450d96b70ec2
eddyFrancois <eddyfrancois85@gmail.com>
2023-11-16 18:44:17 -0500

    bbb bbb bbb

--- web/controlleur/achatServeur.php
+++ web/controlleur/achatServeur.php
@@ -1,27 +1,5 @@
 <?php
 session_start();
-<<<<<<< HEAD
-$id_client;
-$id_article;
-$quantite;
-$date;
-
-if (isset($_POST['id_client'])){
-    $id_client = $_POST['id_client'];
-}
-
-if (isset($_POST['id_article'])){
-    $id_article = $_POST['id_article'];
-}
-
-if (isset($_POST['quantite'])){
-    $quantite = $_POST['quantite'];
-}
-
-if (isset($_POST['date'])){
-    $date = $_POST['date'];
-}
-=======
 
 if (isset($_GET['page']) && isset($_GET['dir'])) {
     $page = $_GET['page'];
@@ -31,7 +9,6 @@ $id_clients = $_POST['id_clients'];
 $id_articles = $_POST['id_articles'];
 $quantite = $_POST['quantite'];
 $date = $_POST['date'];
->>>>>>> 03cd89448c240d2cb7ea7bb6f3088e855ea15d58
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     if(lister()>0){
@@ -41,13 +18,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     $action = $_POST['action'];
     switch($action){
         case "enregistrer" : 
-<<<<<<< HEAD
-            if (enregistrer($id_client, $id_article, $quantite, $date) > 0){
-                header("location: ../achat/achat.php");
-=======
             if (enregistrer($id_clients, $id_articles, $quantite, $date, $user) > 0){
                 header("location : ../achat/achat.php");
->>>>>>> 03cd89448c240d2cb7ea7bb6f3088e855ea15d58
             }
         exit();
           case "modifier":;
@@ -55,11 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     }
 
 }
-<<<<<<< HEAD
- function  enregistrer($id_client, $id_article, $quantite, $date){
-=======
  function  enregistrer($id_clients, $id_articles, $quantite, $date, $user){
->>>>>>> 03cd89448c240d2cb7ea7bb6f3088e855ea15d58
      $etat = 0;
      //etablir la connexion
      $conn = mysqli_connect('localhost', $user, $user, 'projet');
