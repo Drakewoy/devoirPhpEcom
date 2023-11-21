96c282b761f50a806c9f474327019e4bae2026c3
Drakewoy <lainedieunineken19@gmail.com>
2023-11-21 11:22:41 -0500

    last

--- web/client/ajou_client.php
+++ web/client/ajou_client.php
@@ -6,29 +6,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<<<<<<< HEAD
-<fieldset>
-    <legend>Enregistrer Un Client</legend>
-    <form action="../controlleur/clientServeur.php?" method="post">
-        <input type="hidden" name="action" value="enregistrer">
-        <label for="nom">Nom</label>
-        <input type="text" name="nom"><br><br>
-        <label for="prenom">Prenom</label>
-        <input type="text" name="prenom"><br><br>
-        <label for="adresse">Adresse</label>
-        <textarea name="adresse" id="adresse" cols="30" rows="10"></textarea><br><br>
-        <label for="codepostal">Code postal</label>
-        <input type="text" name="codepostal"><br><br>
-        <label for="ville">Ville</label>
-        <input type="text" name="ville"><br><br>
-        <label for="pays">Pays</label>
-        <input type="text" name="pays"><br><br>
-        <label for="telephone">Telephone</label>
-        <input type="text" name="telephone"><br><br>
-        <input type="submit" value="Enregistrer">
-    </form>
-=======
->>>>>>> 8bd27a014349a99c37ed5e537031d4df24a9b5d3
 
 <body>
     <div class="page">
--- web/client/client.php
+++ web/client/client.php
@@ -5,46 +5,6 @@
     <link rel="stylesheet" href="../css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-
-    <title>Liste Clients</title>
-
-</head>
-
-<body>
-    <table border="1">
-        <tr>
-            <th>Numero</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>Adresse</th>
-            <th>Code postal</th>
-            <th>Ville</th>
-            <th>Pays</th>
-            <th>Telephone</th>
-        </tr>
-        <?php
-        session_start();
-        if (isset($_SESSION['listClient'])) {
-            foreach ($_SESSION['listClient'] as $row) {
-                echo "<tr>";
-                echo "<td>" . $row['numero'] . "</td>";
-                echo "<td>" . $row['nom'] . "</td>";
-                echo "<td>" . $row['prenom'] . "</td>";
-                echo "<td>" . $row['adresse'] . "</td>";
-                echo "<td>" . $row['codepostal'] . "</td>";
-                echo "<td>" . $row['ville'] . "</td>";
-                echo "<td>" . $row['pays'] . "</td>";
-                echo "<td>" . $row['telephone'] . "</td>";
-                echo "</tr>";
-            }
-        }
-        ?>
-    </table>
-</body>
-
-</html>
-=======
     <title>Liste Clients</title>
 </head>
 
@@ -85,4 +45,3 @@
 </body>
 
 </html>
->>>>>>> 8bd27a014349a99c37ed5e537031d4df24a9b5d3
